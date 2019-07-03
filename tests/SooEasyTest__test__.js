d462b85307bcd20518c98d87f50e9cc22e1f2267
Sergei Trishkin <43265300+grdvsng@users.noreply.github.com>
2019-07-03 10:04:52 +0300

    Update SooEasyTest__test__.js

--- tests/SooEasyTest__test__.js
+++ tests/SooEasyTest__test__.js
@@ -1,3 +1,14 @@
+
+function foo(arg)
+{
+	return arg + 1; 
+}
+
+function boo(arg1, arg2)
+{
+	return arg1 + arg2
+}
+
 var test    = new SooEasyTest(false),
 	testing = [
 		{
@@ -26,12 +37,12 @@ var test    = new SooEasyTest(false),
 
 
 
-test.run(foo, 1);				     // Without error and 1 parametr
-test.run(boo, [1, 2]);				 // Without error and many parametrs
-test.run(foo, 1, false, 2);			 // Without error, parametr and expected result
-test.run(boo, [1, 2], false, 3);	 // Without error, parametrs and expected result
-test.run(foo, 1, false, 3);			 // With error, parametr and expected result
-test.run(boo, [1, 2], false, 1);	 // With error, parametrs and expected result
+test.run(foo, 1);		     // Without error and 1 parametr
+test.run(boo, [1, 2]);		     // Without error and many parametrs
+test.run(foo, 1, false, 2);	     // Without error, parametr and expected result
+test.run(boo, [1, 2], false, 3);     // Without error, parametrs and expected result
+test.run(foo, 1, false, 3);	     // With error, parametr and expected result
+test.run(boo, [1, 2], false, 1);     // With error, parametrs and expected result
 test.runManyTest(testing)            // Use book with many test
 
 test.printResults();
